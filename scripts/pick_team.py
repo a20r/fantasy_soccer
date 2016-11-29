03cd4713c0c71a8911dfbf3a69891dd138275bb1
Alexander Wallar <alex@wallar.me>
2016-11-29 17:22:11 -0500

    team selected, i think

--- scripts/pick_team.py
+++ scripts/pick_team.py
@@ -1,7 +1,37 @@
 
 import pandas as pd
 import tabulate
-from gurobipy import Model, quicksum, GRB
+from gurobipy import Model, quicksum, GRB, tuplelist
+
+
+def per_pos_team_constr(m, tcodes, team_pos):
+    for tc in tcodes:
+        for i in xrange(1, 5):
+            ps = list(team_pos.select("*", i, tc))
+            m.addConstr(quicksum([p[0] for p in ps]) <= 2)
+
+
+def total_team_constr(m, teams):
+    for team in teams:
+        m.addConstr(quicksum(team) <= 3)
+
+
+def total_cost_constr(m, xs, players):
+    costs = list()
+    for i in xrange(players.shape[0]):
+        pcost = players.iloc[i]["now_cost"]
+        costs.append(xs[i] * pcost)
+    m.addConstr(quicksum(costs) <= 1000)
+
+
+def player_objective(player, thresh):
+    obj = 0
+    chance_of_playing = player["chance_of_playing_next_round"]
+    if chance_of_playing >= thresh:
+        ppg = player["points_per_game"]
+        form = player["form"]
+        obj = -ppg * form
+    return obj
 
 
 def pick_team():
@@ -9,43 +39,34 @@ def pick_team():
     m = Model("pick_team")
     m.params.OutputFlag = 0
     xs = list()
-    gks = list()
-    defs = list()
-    mids = list()
-    fors = list()
+    gks, defs, mids, fors = [], [], [], []
     assigns = [gks, defs, mids, fors]
     teams = [[] for _ in xrange(players.shape[0])]
+    team_pos = tuplelist()
+    tcodes = set()
 
     for i in xrange(players.shape[0]):
-        obj = 0
-        chance_of_playing = players.iloc[i]["chance_of_playing_next_round"]
-        if chance_of_playing >= 75:
-            ppg = players.iloc[i]["points_per_game"]
-            form = players.iloc[i]["form"]
-            obj = -ppg * form
+        obj = player_objective(players.iloc[i], 75)
         ptype = players.iloc[i]["element_type"]
         tcode = players.iloc[i]["team_code"]
         v = m.addVar(vtype=GRB.BINARY, obj=obj)
         assigns[ptype - 1].append(v)
         teams[tcode - 1].append(v)
+        team_pos.append((v, ptype, tcode))
+        tcodes.add(tcode)
         xs.append(v)
 
     m.update()
 
-    costs = list()
-    for i in xrange(players.shape[0]):
-        pcost = players.iloc[i]["now_cost"]
-        costs.append(xs[i] * pcost)
+    per_pos_team_constr(m, tcodes, team_pos)
+    total_cost_constr(m, xs, players)
+    total_team_constr(m, teams)
 
-    m.addConstr(quicksum(costs) <= 1000)
     m.addConstr(quicksum(xs) == 15)
     m.addConstr(quicksum(gks) == 2)
     m.addConstr(quicksum(defs) == 5)
     m.addConstr(quicksum(mids) == 5)
     m.addConstr(quicksum(fors) == 3)
-
-    for team in teams:
-        m.addConstr(quicksum(team) <= 3)
 
     m.optimize()
 
@@ -61,16 +82,11 @@ def select_players(team):
     m = Model("select_players")
     m.params.OutputFlag = 0
     xs = list()
-    gks = list()
-    defs = list()
-    mids = list()
-    fors = list()
+    gks, defs, mids, fors = [], [], [], []
     assigns = [gks, defs, mids, fors]
 
     for player in team:
-        obj = 0
-        if player["chance_of_playing_next_round"] > 75:
-            obj = -player["points_per_game"] * player["form"]
+        obj = player_objective(player, 100)
         ptype = player["element_type"]
         v = m.addVar(vtype=GRB.BINARY, obj=obj)
         assigns[ptype - 1].append(v)
@@ -80,9 +96,9 @@ def select_players(team):
 
     m.addConstr(quicksum(xs) == 11)
     m.addConstr(quicksum(gks) == 1)
-    m.addConstr(quicksum(defs) == 4)
-    m.addConstr(quicksum(mids) == 4)
-    m.addConstr(quicksum(fors) == 2)
+    m.addConstr(quicksum(defs) >= 3)
+    # m.addConstr(quicksum(mids) == 4)
+    m.addConstr(quicksum(fors) >= 1)
 
     m.optimize()
 
